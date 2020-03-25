77ad1b8e2adf16c44795bbd8bbee414d5baa4d5c
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-03-25 20:43:22 -0300

    ServiceController implemented

---
+++ be/src/controllers/ServiceController.js
@@ -0,0 +1,40 @@
+const connection = require('../database/connection');
+
+module.exports = {
+  async store(req, res) {
+    const { title, description, value } = req.body;
+    const enterprise_id = req.headers.authorization;
+
+    // const result = await connection('services').insert({
+    const [id] = await connection('services').insert({
+      title,
+      description,
+      value,
+      enterprise_id
+    });
+    // const id = result[0];
+
+    return res.json({ id });
+  },
+
+  async index(req, res) {
+    const services = await connection('services').select('*');
+
+    return res.json(services);
+  },
+
+  async delete(req, res) {
+    const { id } = req.params;
+    const enterprise_id = req.headers.authorization;
+
+    const service = await connection('services').where('id', id).select('enterprise_id').first();
+
+    if (service.enterprise_id !== enterprise_id) {
+      return res.status(401).json({ error: 'Operation not permitted.' });
+    }
+
+    await connection('services').where('id', id).delete();
+
+    return res.status(204).send();
+  }
+}
--- be/src/routes.js
+++ be/src/routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const EnterpriseController = require('./controllers/EnterpriseController');
+const ServiceController = require('./controllers/ServiceController');
 
 const routes = express.Router();
 
+// Enterprise routes
 routes.get('/enterprises', EnterpriseController.index);
-
 routes.post('/enterprises', EnterpriseController.store);
+// Service routes
+routes.post('/services', ServiceController.store);
+routes.get('/services', ServiceController.index);
+routes.delete('/services/:id', ServiceController.delete);
 
 module.exports = routes;
