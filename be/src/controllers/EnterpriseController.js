7a794e2ca6e7c2729b5db83d045fa991a292cc98
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-03-26 01:33:21 -0300

    Improv: enterprise update method implemented

--- be/src/controllers/EnterpriseController.js
+++ be/src/controllers/EnterpriseController.js
@@ -24,5 +24,33 @@ module.exports = {
     const enterprises = await connection('enterprises').select('*');
 
     return res.json(enterprises);
+  },
+
+  async update(req, res) {
+    const { name, email } =  req.body;
+
+    const enterpriseData = await connection('enterprises').where('id', req.userIdToken).select('name', 'email');
+
+    const [enterprise] = enterpriseData;
+
+    if (name && name === enterprise.name) {
+      const enterpriseExists = await connection('enterprises').where('name', name).select('*');
+
+      if (enterpriseExists) {
+        return res.status(400).json({ error: "User already exists. No name changes." });
+      }
+    }
+
+    if (email && email === enterprise.email) {
+      const enterpriseExists = await connection('enterprises').where('email', email).select('*');
+
+      if (enterpriseExists) {
+        return res.status(400).json({ error: "User already exists. No email changes." });
+      }
+    }
+
+    await connection('enterprises').update({ name, email }).where('id', req.userIdToken);
+
+    return res.json({ success: true });
   }
 }
