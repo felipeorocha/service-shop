6fa75d33fc088d7baffbd6a7bd3a5d0a83d65e7b
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-03-25 22:11:57 -0300

    Cors config

--- be/src/index.js
+++ be/src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const app = express();
 
+// app.use(cors({
+//   origin: 'http://front_end_domain_origin'
+// })); // PROD
+app.use(cors()); // DEV
 app.use(express.json());
 app.use(routes);
 
