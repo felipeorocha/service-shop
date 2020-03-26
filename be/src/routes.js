3f0cfc4445707e6e0368c2f79b66d5e48689b6c9
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-03-26 01:35:58 -0300

    Improv: Add auth middleware to enterprise update route

--- be/src/routes.js
+++ be/src/routes.js
@@ -4,13 +4,18 @@ const ServiceController = require('./controllers/ServiceController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 
+const authMiddleware = require('./middlewares/auth');
+
 const routes = express.Router();
+
+// routes.use(authMiddleware);
 
 // Login route
 routes.post('/sessions', SessionController.store);
 // Enterprise routes
 routes.get('/enterprises', EnterpriseController.index);
 routes.post('/enterprises', EnterpriseController.store);
+routes.put('/enterprises', authMiddleware, EnterpriseController.update);
 // Service routes
 routes.post('/services', ServiceController.store);
 routes.get('/services', ServiceController.index);
